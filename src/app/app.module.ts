fe7343fb7ede972d48a67ad04e43121c1147595f
Pedro Camejo <camejodiazpedro@gmail.com>
2022-05-03 20:14:30 +0100

    maquetación-booking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BookingComponent } from './components/booking/booking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
