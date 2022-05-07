7b429435c6c7e46339156dccf3ccbdaf563b8caf
MaximoDM <rtp.iea2010@gmail.com>
2022-05-07 18:49:23 +0100

    added a basic but functionally home/index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { BookingFormComponent } from './components/bookingForm/booking.component';
 import { CompanyComponent } from './components/company/company.component';
 import { BookingsComponent } from './components/bookings/bookings.component'
-
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: ':company', component: CompanyComponent},
   {path: ':company/booking', component: BookingFormComponent},
   {path: ':company/:bookingToken', component: BookingsComponent}
