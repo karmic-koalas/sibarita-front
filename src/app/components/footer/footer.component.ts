1f9247ffac774732f8f3998571a8bb3528273429
Erik Eduardo <erikeah@protonmail.com>
2022-05-13 17:56:11 +0100

    wip: footer is finished

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  constructor(public redirect: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  goto(direction: string) {
+    this.redirect.navigate([direction]);
   }
 
+  ngOnInit(): void {}
 }
