e7a9241ae8360c2cbef17ad1ed96dddcd86e4e09
Pedro Camejo <camejodiazpedro@gmail.com>
2022-05-22 12:43:45 +0100

    home-component: some styles fixed. controlpanel-component: little bit of styles and fixed buttons behavior

--- src/app/components/controlpanel/controlpanel.component.ts
+++ src/app/components/controlpanel/controlpanel.component.ts
@@ -13,6 +13,9 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./controlpanel.component.scss'],
 })
 export class ControlpanelComponent implements OnInit {
+  btnPersonalInformation = document.getElementById('#personal-information-button') as HTMLButtonElement | null;
+  btnBookingButton = document.getElementById('#bookings-button') as HTMLButtonElement | null;
+
   company: any = {};
   address: any = {};
   contact: any = {};
@@ -118,22 +121,19 @@ export class ControlpanelComponent implements OnInit {
     });
   }
 
+  
+
   isPersonalInformationButtonPressed() {
-    if (this.personalInformationChecked === false) {
-      this.personalInformationChecked = true;
-      this.bookingsChecking = false;
-    } else {
-      this.personalInformationChecked = false;
-    }
+    this.personalInformationChecked = true;
+    if (this.btnPersonalInformation != null) this.btnPersonalInformation.disabled = true;
+    if (this.btnBookingButton != null) this.btnBookingButton.disabled = false;
+    this.bookingsChecking = false;
   }
 
   isBookingsButtonPressed() {
-    if (this.bookingsChecking === false) {
-      this.bookingsChecking = true;
-      this.personalInformationChecked = false;
-    } else {
-      this.bookingsChecking = false;
-      this.personalInformationChecked = true;
-    }
+    this.bookingsChecking = true;
+    if (this.btnPersonalInformation != null) this.btnPersonalInformation.disabled = false;
+    if (this.btnBookingButton != null) this.btnBookingButton.disabled = true;
+    this.personalInformationChecked = false;
   }
 }
